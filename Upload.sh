459e267c976a023ce648d4f37d6e5e90e0bb14a7
Robert Heller <heller@deepsoft.com>
2022-10-19 22:58:02 -0400

    Fix Upload.sh

--- Upload.sh
+++ Upload.sh
@@ -18,7 +18,7 @@ python $HOME/.arduino15/packages/esp32/tools/esptool_py/3.3.0/esptool.py \
     --flash_freq 80m --flash_size detect 0xe000 \
     $HOME/.arduino15/packages/esp32/hardware/esp32/*/tools/partitions/boot_app0.bin \
     0x1000 \
-    $HOME/.arduino15/packages/esp32/hardware/esp32/*/tools/sdk/bin/bootloader_qio_80m.bin \
-    0x10000 $BUILDDIR/`basename $ino`.bin \
+    $HOME/.arduino15/packages/esp32/hardware/esp32/*/tools/sdk/esp32/bin/bootloader_qio_80m.bin \
+    0x10000 $BUILDPATH/`basename $ino`.bin \
     0x8000 \
-    $BUILDDIR/`basename $ino`.partitions.bin
+    $BUILDPATH/`basename $ino`.partitions.bin
